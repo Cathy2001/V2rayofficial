26ca8cd71f096805d4a7a7b253f73d578c634696
ComeBey <46746974+ComeBey@users.noreply.github.com>
2019-02-25 12:42:03 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# This file is accessible as https://install.direct/go.sh
-# Original source is located at github.com/v2ray/v2ray-core/release/install-release.sh
+# This file is accessible as https://raw.githubusercontent.com/ComeBey/V2rayofficial/master/v2ray.sh
 
 # If not specify, default meaning of return value:
 # 0: Success
